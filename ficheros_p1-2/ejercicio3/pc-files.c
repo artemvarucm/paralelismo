fc906e94a5c1c62ca26db1e6b6476acf2acf4f75
Artem <artemvar@ucm.es>
2025-02-11 17:36:58 +0000

    PR2: Ejercicio 3 funciona

--- ficheros_p1-2/ejercicio3/pc-files.c
+++ ficheros_p1-2/ejercicio3/pc-files.c
@@ -4,18 +4,18 @@
 #include <err.h>
 #include <unistd.h> 
 #include <pthread.h>
-#include <semaphore.h>
 #include <fcntl.h>
 
-char *loadline(int fd);
 void* producir(void* arg);
 void* consumir(void* arg);
+int consumirLinea(FILE *fd);
+void producirLinea(char* linea);
 
 #define MAX_LINE_LENGTH 256
 #define MAX_SBUFFER_SIZE 4
 char* shared_buffer[MAX_SBUFFER_SIZE];
 
-pthread_mutex_t mutex;
+pthread_mutex_t lock;
 pthread_cond_t cond_prod, cond_cons;
 
 
@@ -48,136 +48,106 @@ int main(int argc, char* argv[])
 		output = "out.txt";
 	}
 
-	int fdIn = STDIN_FILENO;
+	FILE* fdIn = stdin;
 	if (input) {
-		fdIn = open(input, O_RDONLY);
-		if (fdIn < 0) {
-			perror("Error al abrir el archivo de entrada");
+		fdIn = fopen(input, "r");
+		if (fdIn == NULL) {
+			perror("Error al abrir el archivo de entrada\n");
 			return 1;
 		}
 	}
 
-	int fdOut = open(output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fdOut < 0) {
-        perror("Error al abrir el archivo de salida");
+	FILE* fdOut = fopen(output, "w");
+    if (fdOut == NULL) {
+        perror("Error al abrir el archivo de salida\n");
         return 1;
     }
 
-	pthread_mutex_init(&mutex, NULL);
-	pthread_create(&productor, NULL, producir, &fdIn);
-	pthread_create(&consumidor, NULL, consumir, &fdOut);
+	if (pthread_mutex_init(&lock, NULL) != 0) { 
+        printf("\n mutex init has failed\n"); 
+        return 1; 
+    } 
 
-	pthread_join(&productor, NULL);
-	pthread_join(&consumidor, NULL);
+	pthread_create(&productor, NULL, producir, fdIn);
+	pthread_create(&consumidor, NULL, consumir, fdOut);
 
-	close(fdOut);
+	pthread_join(productor, NULL);
+	pthread_join(consumidor, NULL);
+
+	pthread_mutex_destroy(&lock);
+
+	fclose(fdOut);
 	if (input) {
-		close(fdIn);
+		fclose(fdIn);
 	}
 
 	return 0;
 }
 
-
 void* producir(void* arg) {
-	int fd = *(int*)arg;
-	char *linea;
+	FILE* fd = (FILE*) arg;
+	char* linea = (char* )malloc(MAX_LINE_LENGTH);
 
-	pthread_mutex_lock(&mutex);
-
-	while(nr_items==MAX_SBUFFER_SIZE){
-		pthread_cond_wait(&cond_prod, &mutex);
+	while(fgets(linea, MAX_LINE_LENGTH, fd) != NULL){
+		//printf("%s", linea);
+		producirLinea(linea);
 	}
-
-	while((linea = loadline(fd)) != NULL){
-		char *p = malloc(strlen(linea) + 1);
-		strcpy(p, linea);
-
-		shared_buffer[widx] = p;
-
-		free(linea);
-
-		widx = (widx + 1) % MAX_SBUFFER_SIZE;
-		nr_items++;
-
-		pthread_cond_signal(&cond_cons);
-		pthread_mutex_unlock(&mutex);
-	}
-
-	pthread_mutex_lock(&mutex);
-
-	while(nr_items==MAX_SBUFFER_SIZE){
-		pthread_cond_wait(&cond_prod, &mutex);
-	}
-	shared_buffer[widx] = NULL;
-	pthread_cond_signal(&cond_cons);
-	pthread_mutex_unlock(&mutex);
-	
-	
+	producirLinea(NULL);
 
     return NULL;
 }
 
 
 void* consumir(void* arg) {
-	int fd = *(int*)arg;
-	char *linea;
-	
-	while (1) {
-		pthread_mutex_lock(&mutex);
-		while(nr_items==0){
-			pthread_cond_wait(&cond_cons, &mutex);
-		}
-		linea = shared_buffer[ridx];
-        
-		if (linea==NULL){
-			pthread_mutex_unlock(&mutex); 
-			pthread_cond_signal(&cond_prod);
-			break;
-		}
-
-		int len = strlen(linea) + 1;
-		if (write(fd, linea, len) < len) {
-            perror("Error al escribir en el archivo");
-            return 1;
-        }
-
-		free(linea);
-
-		ridx = (ridx + 1) % MAX_SBUFFER_SIZE;
-		nr_items--;
-
-		pthread_cond_signal(&cond_prod);
-		pthread_mutex_unlock(&mutex);
+	FILE* fd = (FILE*) arg;
+	while(consumirLinea(fd)){
 	}
 
     return NULL;
 }
-char *loadline(int fd)
-{
-    int count = 0;
-    char c;
-    
-    // Contar caracteres hasta encontrar '\n'
-    while (read(fd, &c, sizeof(char)) == 1 && c != '\n') {
-        count++;
-    }
 
-    if (count == 0) {
-        return NULL;
-    }
-    
-    // Retroceder el puntero del archivo
-    lseek(fd, -(count + 1), SEEK_CUR);
-    
-    // Reservar memoria para la cadena
-    char *str = (char*)malloc(count + 1);
-    
-	if (str) {
-		// Volvemos a leer
-        read(fd, str, count + 1);
-    	return str;
-    }
+void producirLinea(char* linea) {
+	pthread_mutex_lock(&lock);
+	while(nr_items == MAX_SBUFFER_SIZE){
+		pthread_cond_wait(&cond_prod, &lock);
+	}
+	shared_buffer[widx] = (char* )malloc(MAX_LINE_LENGTH);
+	if (linea == NULL) {
+		shared_buffer[widx] = NULL;
+	} else {
+		strcpy(shared_buffer[widx], linea);
+	}
 
-	return NULL;	
+	widx = (widx + 1) % MAX_SBUFFER_SIZE;
+	nr_items++;
+	
+	pthread_cond_signal(&cond_cons);
+	pthread_mutex_unlock(&lock);
+}
+
+int consumirLinea(FILE *fd) {
+	pthread_mutex_lock(&lock);
+	
+	while(nr_items==0){
+		pthread_cond_wait(&cond_cons, &lock);
+	}
+	char *linea = shared_buffer[ridx];
+	int noParar = (linea != NULL);
+	if (noParar) {
+		int len = strlen(linea) + 1;
+		
+		if (fwrite(linea, sizeof(char), len, fd) < len) {
+			perror("Error al escribir en el archivo\n");
+		}
+
+		free(linea);
+	}
+
+	ridx = (ridx + 1) % MAX_SBUFFER_SIZE;
+	nr_items--;
+	
+	pthread_cond_signal(&cond_prod);
+	pthread_mutex_unlock(&lock);
+
+	return noParar;
 }
