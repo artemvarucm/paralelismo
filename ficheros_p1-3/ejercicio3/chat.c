ae5f67262d83e4d517626c8733ad72132d455fb2
MarioBaldocchi <147535172+MarioBaldocchi@users.noreply.github.com>
2025-02-25 11:28:38 +0100

    chat empezado

--- ficheros_p1-3/ejercicio3/chat.c
+++ ficheros_p1-3/ejercicio3/chat.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <err.h>
 #include <pthread.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <semaphore.h>
 #define MAX_CHARS_MSG   128
 
 typedef enum  {
@@ -19,9 +22,84 @@ struct chat_message{
      message_type_t type;	
 };
 
+pthread_t emisor;
+pthread_t receptor;
+
+int fifo_envio, fifo_receptor;
+
+char *usuario, *nombre_otro_usuario;
+
+void *enviar();
+void *recibir();
 
 int main(int argc, char* argv[])
 {
-	/* To be completed */
+     if (argc < 4) {
+          fprintf(stderr, "Uso: %s <usuario> <ruta-fifo-envío> <ruta-fifo-recepción>\n", argv[0]);
+          return 1;
+     }
+
+     //Abrimos los archivos
+     usuario = malloc(strlen(argv[1]) + 1);
+     strcpy(usuario, argv[1]);
+     fifo_envio = open(argv[2], O_WRONLY);
+     fifo_receptor = open(argv[3], O_RDONLY);
+
+     if (fifo_envio == -1 || fifo_receptor == -1) {
+          perror("Error al abrir los FIFOs");
+          return 1;
+     }
+
+     // Crear los dos hilos
+     pthread_create(&emisor, NULL, enviar, NULL);
+     pthread_create(&receptor, NULL, recibir, NULL);
+
+     pthread_join(emisor, NULL);
+     pthread_join(receptor, NULL);
+
+     free(usuario);
+     free(nombre_otro_usuario);
+
+       
 	return 0;
 }
+
+void *enviar() {
+     struct chat_message msg;
+     msg.type = USERNAME_MSG;
+     strncpy(msg.contenido, usuario, MAX_CHARS_MSG);
+     
+     write(fifo_envio, &msg, sizeof(msg));
+     
+     while (fgets(msg.contenido, MAX_CHARS_MSG, stdin) != NULL) {
+         msg.type = NORMAL_MSG;
+         write(fifo_envio, &msg, sizeof(msg));
+     }
+     
+     msg.type = END_MSG;
+     write(fifo_envio, &msg, sizeof(msg));
+     close(fifo_envio);
+     return NULL;
+ }
+
+ void *recibir() {
+     struct chat_message msg;
+     
+     if (read(fifo_receptor, &msg, sizeof(msg)) > 0 && msg.type == USERNAME_MSG) {
+          strncpy(nombre_otro_usuario, msg.contenido, MAX_CHARS_MSG); 
+          printf("%s se ha conectado.\n", nombre_otro_usuario);
+     }
+     
+     while (1) {
+         if (read(fifo_receptor, &msg, sizeof(msg)) > 0) {
+             if (msg.type == NORMAL_MSG) {
+                 printf("%s dice: %s", nombre_otro_usuario, msg.contenido);
+             } else if (msg.type == END_MSG) {
+                 printf("%s ha salido del chat.\n", nombre_otro_usuario);
+                 break;
+             }
+         }
+     }
+     close(fifo_receptor);
+     return NULL;
+ }
