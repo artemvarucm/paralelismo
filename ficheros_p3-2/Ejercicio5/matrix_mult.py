8530c359003b6b9615f636bf65cd22d746c33927
Artem <artemvar@ucm.es>
2025-04-20 16:56:26 +0200

    multiplicacion 3.2 - falta probar

--- ficheros_p3-2/Ejercicio5/matrix_mult.py
+++ ficheros_p3-2/Ejercicio5/matrix_mult.py
@@ -17,14 +17,16 @@ def multiply_matrices_flat(A, B, N):
 @njit
 def multiply_matrices_gpu(A, B, N):
     result = np.zeros(N * N)
-    with openmp("target map(to: result) map(to: A) map(to: B)"):
-        with openmp("parallel for"):
-            for i in range(N):
-                for j in range(N):
-                    sum_val = 0.0
-                    for k in range(N):
-                        sum_val += A[i * N + k] * B[k * N + j]
-                    result[i * N + j] = sum_val
+    with openmp("target teams distribute parallel for collapse(2) \
+                map(to: A[:N*N], B[:N*N]) \
+                map(from: result[:N*N])"):
+        for i in range(N):
+            for j in range(N):
+                sum_val = 0.0
+                for k in range(N):
+                    sum_val += A[i * N + k] * B[k * N + j]
+                result[i * N + j] = sum_val
+    
     return result
 
 # Size of the matrices (you can adjust to test performance)
@@ -41,7 +43,7 @@ B = np.random.rand(N * N)
 start_time = time.time()
 
 # Perform multiplication
-y = multiply_matrices_flat(A, B, N)
+y = multiply_matrices_gpu(A, B, N)
 
 # Record end time
 end_time = time.time()
@@ -51,21 +53,3 @@ print(f"Multiplication time: {end_time - start_time:.4f} seconds")
 
 # Print the reduction (sum of all elements) to verify result
 print(f"Reduction (sum of result matrix): {np.sum(y):.4f}")
-
-### GPU
-
-# Record start time
-start_time = time.time()
-
-# Perform multiplication
-y_gpu = multiply_matrices_gpu(A, B, N)
-
-# Record end time
-end_time = time.time()
-
-print("GPU")
-# Display execution time
-print(f"Multiplication time: {end_time - start_time:.4f} seconds")
-
-# Print the reduction (sum of all elements) to verify result
-print(f"Reduction (sum of result matrix): {np.sum(y_gpu):.4f}")
